4a4eef819468e959d3c5cf77a494ec30fe2e8165
LeoPavlovski <lp29379@seeu.edu.mk>
2023-06-18 00:03:30 +0200

    feat:requests

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->group( function (){
+Route::middleware(['auth:sanctum','api.throttle:10,1'])->group( function (){
     Route::get('/getActors',[ActorController::class,'index']);
     Route::get('/getActor/{actor}',[ActorController::class,'show']);
     Route::post('/createActor',[ActorController::class,'store']);
