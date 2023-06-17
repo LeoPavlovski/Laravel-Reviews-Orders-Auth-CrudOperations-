e4a67d60c055710ee4fb1a55c4863eb3729f767e
LeoPavlovski <lp29379@seeu.edu.mk>
2023-06-17 14:58:04 +0200

    feat:[Controllers finished]

---
+++ app/Models/MovieTypes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+//   Schema::create('movies_types', function (Blueprint $table) {
+//            $table->id();
+//            $table->string('types');
+//            $table->timestamps();
+//        });
+//    }
+class MovieTypes extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+      'types'
+    ];
+}
