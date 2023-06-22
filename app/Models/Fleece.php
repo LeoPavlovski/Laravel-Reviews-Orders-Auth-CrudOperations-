1b07e81b8d2d0bffaeb47db3e1a17f66e2ba6be2
LeoPavlovski <lp29379@seeu.edu.mk>
2023-06-22 10:32:35 +0200

    feat: [Added FLEECE MANJAKOT ]

---
+++ app/Models/Fleece.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fleece extends Model
+{
+    use HasFactory;
+    //TODO NEMA PROTECTED OVDE NEMAAAAAAAAAA
+    // FLAGAM KO FLEEECEEE SUIIIIIIIIIII
+    protected $fillable =[
+        'image'
+    ];
+}
