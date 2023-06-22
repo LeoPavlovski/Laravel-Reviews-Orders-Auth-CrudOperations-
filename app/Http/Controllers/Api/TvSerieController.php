9c9196972c5c64f23a1972b860b311f3e01a09cd
LeoPavlovski <lp29379@seeu.edu.mk>
2023-06-22 10:24:37 +0200

    feat: [Added Query]

--- app/Http/Controllers/Api/TvSerieController.php
+++ app/Http/Controllers/Api/TvSerieController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\TVResource;
 use App\Models\Tv;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+
 //"tv_channel"=>'telma',
 //                'episodes'=>'something',
 //                'actor_id'=>1,
@@ -14,13 +16,25 @@ class TvSerieController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     *
+     * 'tv_channel',
+    'episodes',
+    'actor_id',
      */
     public function index()
     {
         $series = Tv::all();
         return TVResource::collection($series);
     }
-
+    public function query(){
+        $query = Tv::query();
+        $tvs = QueryBuilder::for($query)
+            ->allowedFilters('tv_channel','episodes','actor_id')
+            ->allowedSorts('tv_channel','episodes','actor_id')
+            ->allowedIncludes('actor')->get();
+        return TVResource::collection($tvs);
+   }
     /**
      * Store a newly created resource in storage.
      */
